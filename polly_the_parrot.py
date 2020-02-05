e053f7b1f9a523ca13dbbfee46031690fc1a2b4b
Noland Seigler <nolandseigler@gmail.com>
2020-02-04 20:53:12 -0600

    first python program

---
+++ polly_the_parrot.py
@@ -0,0 +1,5 @@
+print("Hello and welcome to my first python program. It happens to be a chatbot.")
+print("Polly The Pirate Parrot Chatbot will repeat everyhting you input.")
+user_input = input("Say something for Polly to Parrot:\n")
+user_input = "Arrrgghh " + user_input
+print("Polly The Pirate Parrot: " + user_input)
