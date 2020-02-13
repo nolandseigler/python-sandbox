aad219039c0fa1bad20e4393c2627fceea5c2e28
Noland Seigler <nolandseigler@gmail.com>
2020-02-12 19:37:23 -0600

    cel to fah using list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -4,3 +4,10 @@ heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 can_ride_coaster = [height for height in heights if height > 161]
 
 print(can_ride_coaster)
+
+celsius = [0, 10, 15, 32, -5, 27, 3]
+
+#convert celsius to fahrenheit using list comprehension
+fahrenheit = [cel * (9 / 5) + 32 for cel in celsius]
+
+print(fahrenheit)
