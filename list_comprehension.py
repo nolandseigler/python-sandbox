57aa9b515d414d132a472269c50a266e67710593
Noland Seigler <nolandseigler@gmail.com>
2020-02-12 19:32:32 -0600

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,4 @@
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+
+#List comprehension. short hand of making a list using a loop and an optional conditional
+can_ride_coaster = [height for height in heights if height > 161]
