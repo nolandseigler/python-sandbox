bdc3900c384ec5c15e770a91021bce733bb5e2e4
Noland Seigler <nolandseigler@gmail.com>
2020-02-17 13:36:30 -0600

    __repr__ defines string representation of an obj so it does not print the mem address but something more useful

--- class.py
+++ class.py
@@ -2,18 +2,37 @@ class Circle:
     pi = 3.14
 
     def __init__(self, diameter):
-        print("Creating circle with diameter {d}".format(d=diameter))
-        # Add assignment for self.radius here:
         self.radius = diameter / 2
 
+    # use __repr__ to define what the string representation of the object should be so it doesnt just print memory
+    # address
+    def __repr__(self):
+        return "Circle with radius {radius}".format(radius=self.radius)
+
+    def area(self):
+        return self.pi * self.radius ** 2
+
     def circumference(self):
-        return 2 * self.pi * self.radius
+        return self.pi * 2 * self.radius
 
 
 medium_pizza = Circle(12)
 teaching_table = Circle(36)
 round_room = Circle(11460)
 
-print(medium_pizza.circumference())
-print(teaching_table.circumference())
-print(round_room.circumference())
+# now prints what __repr__ returns
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
+
+# functions can have instance vars added after init
+# use dir to see all
+# dunders __ are usually built in
+print(dir(5))
+
+
+def this_function_is_an_object(param):
+    return "Anything you'd like."
+
+
+print(dir(this_function_is_an_object))
