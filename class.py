31dfc38fc39822973f7ed987a73e7ad244231c7c
Noland Seigler <nolandseigler@gmail.com>
2020-02-17 13:54:48 -0600

    more work with classes and methods

--- class.py
+++ class.py
@@ -36,3 +36,29 @@ def this_function_is_an_object(param):
 
 
 print(dir(this_function_is_an_object))
+
+
+class Student:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+        self.grades = []
+
+    def add_grade(self, grade):
+        if type(grade) is Grade:
+            self.grades.append(grade)
+
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+
+class Grade:
+    minimum_passing = 65
+
+    def __init__(self, score):
+        self.score = score
+
+
+pieter.add_grade(Grade(100))
