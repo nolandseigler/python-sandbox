87ea4bf30ad3e2edeea07f84c188087fd53bf41b
Noland Seigler <nolandseigler@gmail.com>
2020-02-18 23:17:24 -0600

    franchises are next

--- restaurant_project.py
+++ restaurant_project.py
@@ -9,6 +9,13 @@ class Menu:
         return self.name.capitalize() + "Menu available from " + str(self.start_time) + " to " + str(
             self.end_time) + "."
 
+    def calculate_bill(self, purchased_items):
+        price = 0
+        for key, value in self.items.items():
+            if key in purchased_items:
+                price += value
+        return price
+
 
 brunch_items = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50,
                 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50, }
@@ -24,4 +31,5 @@ early_bird = Menu("early bird", early_bird_items, 1500, 1800)
 dinner = Menu("dinner", dinner_items, 1700, 2300)
 kids = Menu("kids", kids_items, 1100, 2100)
 
-print(brunch)
+# print(brunch)
+print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
