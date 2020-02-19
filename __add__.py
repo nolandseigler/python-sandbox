c8c5c4d12b71fca9a2a4d875abf726e2736bda95
Noland Seigler <nolandseigler@gmail.com>
2020-02-18 21:18:57 -0600

    dunder add. so cool. so dunder

---
+++ __add__.py
@@ -0,0 +1,20 @@
+class Atom:
+    def __init__(self, label):
+        self.label = label
+
+    def __add__(self, other):
+        return Molecule([self, other])
+
+
+class Molecule:
+    def __init__(self, atoms):
+        if type(atoms) is list:
+            self.atoms = atoms
+
+
+sodium = Atom("Na")
+chlorine = Atom("Cl")
+salt = Molecule([sodium, chlorine])
+salt2 = sodium + chlorine
+print(salt)
+print(salt2)
