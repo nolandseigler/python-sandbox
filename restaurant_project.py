fafb09489399c169160f360e2ce7d42822138388
Noland Seigler <nolandseigler@gmail.com>
2020-02-18 22:47:38 -0600

    restaraunt project started

---
+++ restaurant_project.py
@@ -0,0 +1,27 @@
+class Menu:
+    def __init__(self, name, items, start_time, end_time):
+        self.name = name
+        self.items = items
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def __repr__(self):
+        return self.name.capitalize() + "Menu available from " + str(self.start_time) + " to " + str(
+            self.end_time) + "."
+
+
+brunch_items = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50,
+                'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50, }
+early_bird_items = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00,
+                    'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50,
+                    'coffee': 1.50, 'espresso': 3.00, }
+dinner_items = {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00,
+                'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00, }
+kids_items = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00, }
+
+brunch = Menu("brunch", brunch_items, 1100, 1600)
+early_bird = Menu("early bird", early_bird_items, 1500, 1800)
+dinner = Menu("dinner", dinner_items, 1700, 2300)
+kids = Menu("kids", kids_items, 1100, 2100)
+
+print(brunch)
