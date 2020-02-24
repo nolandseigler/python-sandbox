0f79da50664c614cf42649617e008ca61888feb1
Noland Seigler <nolandseigler@gmail.com>
2020-02-23 21:34:07 -0600

    tokenization with nltk

--- file_crazy_args.py
+++ file_crazy_args.py
@@ -9,3 +9,22 @@ def remove(filename, *args, **kwargs):
 
 
 print(remove("text.txt", "generous", "gallant", fond="amused by", Robin="Mr. Robin"))
+
+
+# destructure dictionary with ** when passing in or unpack list or tuple with * when passing in
+def create_product(name, price):
+    print("{} created, being sold for ${}".format(name, price))
+
+
+def create_products(**products_dict):
+    for name, price in products_dict.items():
+        create_product(name, price)
+
+
+new_product_dict = {
+    'Apple': 1,
+    'Ice Cream': 3,
+    'Chocolate': 5,
+}
+
+create_products(**new_product_dict)
