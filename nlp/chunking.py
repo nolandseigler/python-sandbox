1c8fa1d5714c52f16c741e88a4272c70221412d2
Noland Seigler <nolandseigler@gmail.com>
2020-02-24 19:06:33 -0600

    more awesome codecademy content from my course

--- nlp/chunking.py
+++ nlp/chunking.py
@@ -1,6 +1,7 @@
 from nltk import RegexpParser, Tree
 from pos_tagged_oz import pos_tagged_oz
 from vp_chunk_counter import vp_chunk_counter
+from np_chunk_counter import np_chunk_counter
 # from pos_tagged_oz import pos_tagged_oz
 
 # define adjective-noun chunk grammar here
@@ -15,6 +16,24 @@ print(scaredy_cat)
 
 # pretty_print the chunked sentence here
 Tree.fromstring(str(scaredy_cat)).pretty_print()
+
+# STUFFS
+# define noun-phrase chunk grammar here
+chunk_grammar = "NP: {<DT>?<JJ>*<NN>}"
+
+# create RegexpParser object here
+chunk_parser = RegexpParser(chunk_grammar)
+
+# create a for loop through each pos-tagged sentence in pos_tagged_oz here
+np_chunked_oz = [chunk_parser.parse(word) for word in pos_tagged_oz]
+
+
+# store and print the most common np-chunks here
+most_common_np_chunks = np_chunk_counter(np_chunked_oz)
+
+print(most_common_np_chunks)
+
+
 
 # MO STUFFS
 from nltk import RegexpParser
@@ -34,4 +53,5 @@ vp_chunked_oz = [chunk_parser.parse(word) for word in pos_tagged_oz]
 most_common_vp_chunks = vp_chunk_counter(vp_chunked_oz)
 
 print(most_common_vp_chunks)
+
 
