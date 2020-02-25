9d8a79b595568dd3a48348ca9e9faf7981311b2f
Noland Seigler <nolandseigler@gmail.com>
2020-02-24 19:02:57 -0600

    continuing through NLP curric

--- nlp/chunking.py
+++ nlp/chunking.py
@@ -1,5 +1,6 @@
 from nltk import RegexpParser, Tree
-
+from pos_tagged_oz import pos_tagged_oz
+from vp_chunk_counter import vp_chunk_counter
 # from pos_tagged_oz import pos_tagged_oz
 
 # define adjective-noun chunk grammar here
@@ -14,3 +15,23 @@ print(scaredy_cat)
 
 # pretty_print the chunked sentence here
 Tree.fromstring(str(scaredy_cat)).pretty_print()
+
+# MO STUFFS
+from nltk import RegexpParser
+from pos_tagged_oz import pos_tagged_oz
+from vp_chunk_counter import vp_chunk_counter
+
+# define verb phrase chunk grammar here
+chunk_grammar = "VP: {<VB.*><DT>?<JJ>*<NN><RB.?>?}"
+
+# create RegexpParser object here
+chunk_parser = RegexpParser(chunk_grammar)
+
+vp_chunked_oz = [chunk_parser.parse(word) for word in pos_tagged_oz]
+
+
+# store and print the most common vp-chunks here
+most_common_vp_chunks = vp_chunk_counter(vp_chunked_oz)
+
+print(most_common_vp_chunks)
+
